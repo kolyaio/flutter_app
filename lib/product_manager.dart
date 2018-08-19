9e113b9ceedeb5f21184291cb9f01caba5ce048e
Nikolai Ilushko <kolyaxil@gmail.com>
2018-08-19 19:09:45 +0300

    Initial commit
    Learning flutter :-)

---
+++ lib/product_manager.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+import './product_control.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+
+  List<String> _products = [];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(children: [
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: ProductControl(_addProduct),
+      ),
+      Products(_products)
+    ],);
+  }
+
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+   @override
+    void initState() {
+      super.initState();
+
+      _products.add(widget.startingProduct);
+    }
+}
