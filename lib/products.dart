060a41846735a065e390ce132c03d767cb4648a0
Nikolai Ilushko <kolyaxil@gmail.com>
2019-02-11 20:17:51 +0200

    Added price label and location bar

--- lib/products.dart
+++ lib/products.dart
@@ -7,22 +7,48 @@ class Products extends StatelessWidget {
 
   Widget _buildProductItem(BuildContext context, int index) {
     return Card(
-      child: Column(
+        child: Column(
       children: <Widget>[
         Image.asset(products[index]['image']),
         Container(
-          padding: EdgeInsets.only(top: 10.0),
-          child:  Text(products[index]['title'], style: TextStyle(fontSize:26.0, fontWeight: FontWeight.bold, fontFamily: 'Oswald'),),
-        ),
-        ButtonBar(
-          alignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlatButton(
-              child: Text('Details'),
-              onPressed: () => Navigator.pushNamed<bool>(context, '/product/' + index.toString()),
-            )
-          ]
-        )
+            padding: EdgeInsets.only(top: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  products[index]['title'],
+                  style: TextStyle(
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Oswald'),
+                ),
+                SizedBox(
+                  width: 8.0,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor,
+                      borderRadius: BorderRadius.circular(5.0)),
+                  child: Text('\$${products[index]['price'].toString()}',
+                      style: TextStyle(color: Colors.white)),
+                )
+              ],
+            )),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                borderRadius: BorderRadius.circular(4.0)),
+            child: Text('Union Square, San Francisco'),
+            ),
+        ButtonBar(alignment: MainAxisAlignment.center, children: <Widget>[
+          FlatButton(
+            child: Text('Details'),
+            onPressed: () => Navigator.pushNamed<bool>(
+                context, '/product/' + index.toString()),
+          )
+        ])
       ],
     ));
   }
