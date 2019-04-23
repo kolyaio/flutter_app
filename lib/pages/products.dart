78f7c85ab420e4df3e5eb33b35690198d28554c6
Nikolai Ilushko <kolyaxil@gmail.com>
2019-04-23 23:36:34 +0300

    Added icon

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -34,6 +34,12 @@ class ProductsPage extends StatelessWidget {
       ),
       appBar: AppBar(
         title: Text('EasyList'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: ProductManager(products),
     );
